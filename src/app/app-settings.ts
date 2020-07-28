520dfc57552f951c96453f48f2807b262256c11a
hgminerva <hgminerva@gmail.com>
2020-07-28 12:45:38 +0800

    https api

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -3,7 +3,7 @@ import { HttpHeaders } from '@angular/common/http';
 
 export class AppSettings {
     // API URL
-    public defaultAPIURLHost = "http://easycloudhrapi.azurewebsites.net";
+    public defaultAPIURLHost = "https://easycloudhrapi.azurewebsites.net";
     // public defaultAPIURLHost = "https://localhost:44369";
 
     // URL Encoded Options
