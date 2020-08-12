56f691edac4aa7ebc4d7aee85e123cbca0233d02
Oliver Enciso <oliverenciso07@gmail.com>
2020-08-12 20:12:06 +0800

    Shift Code List Fixes

--- src/app/software/shift-code-list/shift-code-list/shift-code-list.component.ts
+++ src/app/software/shift-code-list/shift-code-list/shift-code-list.component.ts
@@ -28,6 +28,7 @@ export class ShiftCodeListComponent implements OnInit {
 
   async ngOnInit() {
     await this.GetShiftCodeListData();
+    await this.CreateCboShowNumberOfRows();
   }
 
   public listShiftCodeObservableArray: ObservableArray = new ObservableArray();
@@ -42,6 +43,42 @@ export class ShiftCodeListComponent implements OnInit {
   private DeleteShiftCodeSubscription: any;
 
   public buttonDisabled: boolean = false;
+
+  public _createCboShowNumberOfRows: ObservableArray = new ObservableArray();
+
+  public CreateCboShowNumberOfRows(): void {
+    for (var i = 0; i <= 4; i++) {
+      var rows = 0;
+      var rowsString = "";
+      if (i == 0) {
+        rows = 15;
+        rowsString = "Show 15";
+      } else if (i == 1) {
+        rows = 50;
+        rowsString = "Show 50";
+      } else if (i == 2) {
+        rows = 100;
+        rowsString = "Show 100";
+      } else if (i == 3) {
+        rows = 150;
+        rowsString = "Show 150";
+      } else {
+        rows = 200;
+        rowsString = "Show 200";
+      }
+
+      this._createCboShowNumberOfRows.push({
+        rowNumber: rows,
+        rowString: rowsString
+      });
+    }
+  }
+
+  public CboShowNumberOfRowsOnSelectedIndexChanged(): void {
+    this.listShiftCodeCollectionView.pageSize = this.listPageIndex;
+    this.listShiftCodeCollectionView.refresh();
+    this.listShiftCodeCollectionView.refresh();
+  }
 
   private async GetShiftCodeListData() {
     this.listShiftCodeObservableArray = new ObservableArray();
