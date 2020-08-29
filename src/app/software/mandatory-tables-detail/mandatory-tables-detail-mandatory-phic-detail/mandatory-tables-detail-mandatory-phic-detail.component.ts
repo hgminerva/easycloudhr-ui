9c0d37ad5bc1e6a2c52e7d9e85aafa48fbfd6c8d
Oliver Enciso <oliverenciso07@gmail.com>
2020-08-28 22:52:40 -0700

    Mandatory PHIC Input Fixes

--- src/app/software/mandatory-tables-detail/mandatory-tables-detail-mandatory-phic-detail/mandatory-tables-detail-mandatory-phic-detail.component.ts
+++ src/app/software/mandatory-tables-detail/mandatory-tables-detail-mandatory-phic-detail/mandatory-tables-detail-mandatory-phic-detail.component.ts
@@ -16,6 +16,7 @@ export class MandatoryTablesDetailMandatoryPhicDetailComponent implements OnInit
   ) { }
 
   public title = '';
+
   public _mandatoryPHIC: any = [];
 
   public inputTypeAmountStart = 'text';
@@ -30,7 +31,10 @@ export class MandatoryTablesDetailMandatoryPhicDetailComponent implements OnInit
     this._mandatoryPHIC.AmountEnd = this._decimalPipe.transform(this.caseData.objData.AmountEnd, "1.2-2");
     this._mandatoryPHIC.EmployeeContributionValue = this._decimalPipe.transform(this.caseData.objData.EmployeeContributionValue, "1.2-2");
     this._mandatoryPHIC.EmployerContributionValue = this._decimalPipe.transform(this.caseData.objData.EmployerContributionValue, "1.2-2");
-    this._mandatoryPHIC.Remarks = this._mandatoryPHIC.Remarks
+    this._mandatoryPHIC.Remarks = this.caseData.objData.Remarks;
+
+    console.log('Edit PHIC ', this.caseData.objData);
+
     console.log(this._mandatoryPHIC);
   }
 
@@ -39,66 +43,79 @@ export class MandatoryTablesDetailMandatoryPhicDetailComponent implements OnInit
   }
 
   public Save(): void {
-    if (this.title == 'Add Mandatory BIR') {
+    if (this.title == 'Add Mandatory PHIC') {
       this.dialogRef.close({ event: 'Add', objData: this._mandatoryPHIC });
     } else {
       this.dialogRef.close({ event: 'Edit', objData: this._mandatoryPHIC });
     }
   }
 
+  public RemoveComma(value: string): string {
+    return value.toString().replace(/,/g, '');
+  }
+
+  AmountStartToNumberType() {
+    this._mandatoryPHIC.AmountStart = this.RemoveComma(this._mandatoryPHIC.AmountStart);
+    this.inputTypeAmountStart = 'number';
+  }
+
   AmountStartFormatValue() {
     this.inputTypeAmountStart = 'text';
 
-    if (this._mandatoryPHIC.AmountStart == '') {
+    if (this._mandatoryPHIC.AmountStart === '') {
       this._mandatoryPHIC.AmountStart = this._decimalPipe.transform(0, "1.2-2");
     } else {
       this._mandatoryPHIC.AmountStart = this._decimalPipe.transform(this._mandatoryPHIC.AmountStart, "1.2-2");
     }
   }
 
-  AmountStartToNumberType() {
-    this.inputTypeAmountStart = 'number';
+  AmountEndToNumberType() {
+    this._mandatoryPHIC.AmountEnd = this.RemoveComma(this._mandatoryPHIC.AmountEnd);
+
+    this.inputTypeAmountEnd = 'number';
   }
 
-  AmountEndFormatValue() {
-    this.inputTypeAmountEnd = 'text';
+  async AmountEndFormatValue() {
+    this.inputTypeAmountEnd = await 'text';
 
-    if (this._mandatoryPHIC.AmountEnd == '') {
+    if (this._mandatoryPHIC.AmountEnd === '') {
       this._mandatoryPHIC.AmountEnd = this._decimalPipe.transform(0, "1.2-2");
     } else {
       this._mandatoryPHIC.AmountEnd = this._decimalPipe.transform(this._mandatoryPHIC.AmountEnd, "1.2-2");
     }
   }
 
-  AmountEndToNumberType() {
-    this.inputTypeAmountEnd = 'number';
+  EmployeeContributionValueToNumberType() {
+    this._mandatoryPHIC.EmployeeContributionValue = this.RemoveComma(this._mandatoryPHIC.EmployeeContributionValue);
+
+    this.inputTypeEmployeeContributionValue = 'number';
   }
 
   EmployeeContributionValueFormatValue() {
     this.inputTypeEmployeeContributionValue = 'text';
 
-    if (this._mandatoryPHIC.EmployeeContributionValue == '') {
+    if (this._mandatoryPHIC.EmployeeContributionValue === '') {
       this._mandatoryPHIC.EmployeeContributionValue = this._decimalPipe.transform(0, "1.2-2");
     } else {
       this._mandatoryPHIC.EmployeeContributionValue = this._decimalPipe.transform(this._mandatoryPHIC.EmployeeContributionValue, "1.2-2");
     }
   }
 
-  EmployeeContributionValueToNumberType() {
-    this.inputTypeEmployeeContributionValue = 'number';
+  EmployerContributionValueToNumberType() {
+    this._mandatoryPHIC.EmployerContributionValue = this.RemoveComma(this._mandatoryPHIC.EmployerContributionValue);
+
+    this.inputTypeEmployerContributionValue = 'number';
   }
 
   EmployerContributionValueFormatValue() {
     this.inputTypeEmployerContributionValue = 'text';
 
-    if (this._mandatoryPHIC.EmployerContributionValue == '') {
+    if (this._mandatoryPHIC.EmployerContributionValue === '') {
       this._mandatoryPHIC.EmployerContributionValue = this._decimalPipe.transform(0, "1.2-2");
     } else {
       this._mandatoryPHIC.EmployerContributionValue = this._decimalPipe.transform(this._mandatoryPHIC.EmployerContributionValue, "1.2-2");
     }
   }
 
-  EmployerContributionValueToNumberType() {
-    this.inputTypeEmployerContributionValue = 'number';
-  }
+
 }
