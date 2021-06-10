5af554a6450c45a33e5ee93ea6efab8ad411f867
Oliver Enciso <oliverenciso07@gmail.com>
2021-06-10 08:37:49 +0800

    EASYCLOUDHR_KIA-117 Demographics-CSV & EASYCLOUDHR_KIA-116 PDF Demographics

--- src/app/software/report/demographics/demographics.component.ts
+++ src/app/software/report/demographics/demographics.component.ts
@@ -37,6 +37,10 @@ export class DemographicsComponent implements OnInit {
 
   public _isProgressBarHidden: boolean = false;
 
+  public _branchListDropdownList: any = [];
+  public _branchListDropdownListSubscription: any;
+  public branch: string = "";
+
   private async GetCompanyDropdownListData() {
     this._companyListDropdownListSubscription = await (await this.reportService.CompanyDropdownList()).subscribe(
       response => {
@@ -50,26 +54,45 @@ export class DemographicsComponent implements OnInit {
         if (this._companyListDropdownListSubscription !== null) this._companyListDropdownListSubscription.unsubscribe();
       }
     );
-    
+
+    await this.GetBranchDropdownListData();
+  }
+
+  private async GetBranchDropdownListData() {
+    this._branchListDropdownListSubscription = await (await this.reportService.BranchList()).subscribe(
+      response => {
+        let results = response;
+        console.log("branch",results);
+        this._branchListDropdownList.push({
+          Id: 0,
+          Value: "ALL"
+        });
+
+        this.branch = "ALL";
+
+        for(var i =0 ; i < results["length"]; i++){
+          this._branchListDropdownList.push(results[i]);
+        }
+      },
+      error => {
+        this._snackBarTemplate.snackBarError(this._snackBar, error.error.Message + " " + error.status);
+      }
+    );
   }
 
 
   private async GetPayrollGroupDropdownListData() {
-
-    
     this.payrollGroupDropdownSubscription = await (await this.reportService.PayrollGroupList()).subscribe(
       response => {
         var results = response;
-        this.payrollGroupListDropdown.push({ Id: 0, Value: 'All'});
+        this.payrollGroupListDropdown.push({ Id: 0, Value: 'ALL'});
 
-        
         if (results["length"] > 0) {
           for (var i = 0; i < results["length"]; i++) {
             this.payrollGroupListDropdown.push(results[i]);
           }
         }
         this.filterPayrollGroup = this.payrollGroupListDropdown[0].Value;
-
         if (this._companyListDropdownListSubscription !== null) this._companyListDropdownListSubscription.unsubscribe();
       },
       error => {
@@ -81,7 +104,7 @@ export class DemographicsComponent implements OnInit {
 
 
   public async printCase() {
-    this._demographicsReportSubscription = (await this.reportService.DemographicsReport(this.companyId, this.filterPayrollGroup )).subscribe(
+    this._demographicsReportSubscription = (await this.reportService.DemographicsReports(this.companyId, this.filterPayrollGroup, this.branch )).subscribe(
       data => {
         var binaryData = [];
         binaryData.push(data);
@@ -100,7 +123,7 @@ export class DemographicsComponent implements OnInit {
 
   public async printCaseCSV() {
     this._isProgressBarHidden = true;
-    (await this.reportService.DemographicsCSVReport(this.companyId)).subscribe(
+    (await this.reportService.DemographicsCSVReport(this.companyId, this.filterPayrollGroup, this.branch)).subscribe(
       data => {
         this._sharedService.generateDemographicsCSV(data, "Demographics", "demographics-report.csv");
         this._isProgressBarHidden = false;
--- src/app/software/report/report.service.ts
+++ src/app/software/report/report.service.ts
@@ -23,32 +23,39 @@ export class ReportService {
     return this.httpClient.get(this.appSettings.defaultAPIURLHost + '/api/pdf/dtr/list', this.appSettings.defaultOptions);
   }
 
-  public async DemographicsReport(companyId: number, payrollGroup: string) {
+  public async DemographicsReports(companyId: number, payrollGroup: string, branch: string) {
 
-    if (payrollGroup == 'All') {
-      let printCaseOptions: any = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-        }),
-        responseType: "blob"
-      };
+    let printCaseOptions: any = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+      }),
+      responseType: "blob"
+    };
 
-      return await this.httpClient.get(this.appSettings.defaultAPIURLHost + '/api/pdf/demographics/report/' + companyId, printCaseOptions);
-    }
-    else {
-      let printCaseOptions: any = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-        }),
-        responseType: "blob"
-      };
+    return await this.httpClient.get(this.appSettings.defaultAPIURLHost + '/api/pdf/demographics/report/' + companyId + '/' + payrollGroup + '/' + branch, printCaseOptions);
+    // if (payrollGroup == 'All') {
+    //   let printCaseOptions: any = {
+    //     headers: new HttpHeaders({
+    //       'Content-Type': 'application/json',
+    //       'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+    //     }),
+    //     responseType: "blob"
+    //   };
 
-      return await this.httpClient.get(this.appSettings.defaultAPIURLHost + '/api/pdf/demographics/by-payroll-group/report/' + companyId + '/' + payrollGroup, printCaseOptions);
-    }
+    //   return await this.httpClient.get(this.appSettings.defaultAPIURLHost + '/api/pdf/demographics/report/' + companyId + '/' + payrollGroup + '/' + branch, printCaseOptions);
+    // }
+    // else {
+    //   let printCaseOptions: any = {
+    //     headers: new HttpHeaders({
+    //       'Content-Type': 'application/json',
+    //       'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+    //     }),
+    //     responseType: "blob"
+    //   };
 
-
+    //   return await this.httpClient.get(this.appSettings.defaultAPIURLHost + '/api/pdf/demographics/by-payroll-group/report/' + companyId + '/' + payrollGroup, printCaseOptions);
+    // }
   }
 
   public async PDFMandatoryReport(mandatory: string, periodId: number, quarter: number, monthnumber: number, companyId: number) {
@@ -104,11 +111,11 @@ export class ReportService {
   // ===================
   // CSV ATM Bank Report
   // ===================
-  public async DemographicsCSVReport(companyId: number) {
+  public async DemographicsCSVReport(companyId: number, payrollGroup: string, branch: string) {
     return await this.httpClient.get(
       this.appSettings.defaultAPIURLHost
       + '/api/report/csv/demographics/report/'
-      + companyId,
+      + companyId + '/' + payrollGroup + '/' + branch,
       this.appSettings.defaultOptions);
   }
 
